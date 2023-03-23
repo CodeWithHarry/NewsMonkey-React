fe4eb253b89a9cce1f462091184481e5dd7991a8
Aviral Khandelwal <aviral.khandelwal@kelltontech.com>
2023-03-23 10:33:11 +0530

    added an interactive loader

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import loading from './loading.gif'
 
-const Spinner = ()=> {
-        return (
-            <div className="text-center">
-                <img className="my-3" src={loading} alt="loading" />
+const Spinner = () => {
+    return (
+        <div class="d-flex justify-content-center">
+            <div class="spinner-grow text-primary" role="status">
+                <span class="visually-hidden">Loading...</span>
             </div>
-        )
+            <div class="spinner-grow text-secondary" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+            <div class="spinner-grow text-success" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+            <div class="spinner-grow text-danger" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+            <div class="spinner-grow text-warning" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+            <div class="spinner-grow text-info" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    )
 }
 
 export default Spinner
