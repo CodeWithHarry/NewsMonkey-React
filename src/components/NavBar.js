ff88683a8b4101e11047631577dd2a052a49f620
dsukhia <dsukhia@entrata.com>
2023-08-03 13:44:40 +0530

    Category menu dynamic from json

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import categories from '../data/categories.json'
 
 const NavBar = () => {
 
@@ -13,16 +14,11 @@ const NavBar = () => {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link className="nav-link" aria-current="page" to="/">Home</Link>
-                            </li>
-                            <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
+                            {categories.map((category) => {
+                                return <li className="nav-item" key={category}>
+                                <Link className="nav-link" to={`/${category}`}>{category.toUpperCase()}</Link>
+                                </li>
+                            })}                            
                         </ul>
                     </div>
                 </div>
