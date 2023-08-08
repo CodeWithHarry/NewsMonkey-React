aa59a41925f5426c730adef81468f590f5ed5143
dsukhia <dsukhia@entrata.com>
2023-08-08 16:49:32 +0530

    added country droupdown and not image picture

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 import noImage from './no-image.png'
 
 const NewsItem = (props)=> {
-        let { title, description, urlToImage, newsUrl, author, publishedAt, source } = props.article;
+        let { title, description, urlToImage, url, author, publishedAt, source } = props.article;
         return (
             <div className="my-3">
                 <div className="card">
@@ -14,7 +14,7 @@ const NewsItem = (props)=> {
                         <h5 className="card-title">{title ? title : "" }</h5>
                         <p className="card-text">{description ? description : ""}</p>
                         <p className="card-text"><small className="text-muted">By {author ? author : "Anonymous"} on  {new Date(publishedAt).toGMTString()}</small></p>
-                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                        <a rel="noreferrer" href={url} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
