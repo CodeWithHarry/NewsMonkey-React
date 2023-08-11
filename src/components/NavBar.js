da046fd81d3c4375aff3b50a4fb09e9646cd9f9e
dsukhia <dsukhia@entrata.com>
2023-08-11 18:22:20 +0530

    bottom to top

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import categories from "../data/categories.json";
 import countries from "../data/countries.json";
+import ScrollButton from "./ScrollButton";
 
 const NavBar = (props) => {
   const capitalizeFirstLetter = (string) => {
@@ -69,7 +70,10 @@ const NavBar = (props) => {
           </div>
         </div>
       </nav>
-      <div className="mt-5 mb-5">{props.children}</div>
+      <div className="container mt-5 mb-5">
+        {props.children}
+        <ScrollButton />
+      </div>
       <footer className="navbar fixed-bottom bg-dark text-white justify-content-center">
           Copyright © {(new Date()).getFullYear()} {process.env.REACT_APP_NAME}. All Rights Reserved.
       </footer>
